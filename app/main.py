4df20067303a972ad2efe614044d6bb02a0770ea
andrespastrana-it <andreserluis@gmail.com>
2025-06-18 12:59:08 -0300

    reafctor: output format to webp

--- app/main.py
+++ app/main.py
@@ -29,16 +29,19 @@ async def crop_face(request: ImageRequest):
 
         top, right, bottom, left = face_locations[0]
 
-        top = max(0, top - (padding+150))
+        top = max(0, top - (padding + 150))
         right = min(image.shape[1], right + padding)
-        bottom = min(image.shape[0], bottom + padding-80)
+        bottom = min(image.shape[0], bottom + padding - 80)
         left = max(0, left - padding)
 
         face_image = image[top:bottom, left:right]
 
-        # Encode cropped face to JPEG
-        _, buffer = cv2.imencode(".jpg", face_image)
-        return StreamingResponse(io.BytesIO(buffer.tobytes()), media_type="image/jpeg")
+        # Encode cropped face to WebP
+        success, buffer = cv2.imencode(".webp", face_image)
+        if not success:
+            raise HTTPException(status_code=500, detail="Failed to encode image to WebP.")
+
+        return StreamingResponse(io.BytesIO(buffer.tobytes()), media_type="image/webp")
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
